bd514107e582b48064b6a7f5f8cf3af196ab7ae5
LostOuija <106669288+LostOuija@users.noreply.github.com>
2024-07-18 21:23:31 -0700

    Update and rename omni_mdh50lm.mk to twrp_mdh50lm.mk

--- twrp_mdh50lm.mk
+++ twrp_mdh50lm.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common Twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from mdh50lm device
 $(call inherit-product, device/lge/mdh50lm/device.mk)
 
 PRODUCT_DEVICE := mdh50lm
-PRODUCT_NAME := omni_mdh50lm
+PRODUCT_NAME := twrp_mdh50lm
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LM-Q730
 PRODUCT_MANUFACTURER := lge
