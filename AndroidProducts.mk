c5123c952e099c539308d049af99bc1a931f426e
LostOuija <jayloveskymmi@gmail.com>
2024-07-31 11:52:07 -0700

    lineage

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,13 @@
 #
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2022 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_mdh50lm.mk
+    $(LOCAL_DIR)/lineage_mdh50lm.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_mdh50lm-user \
-    twrp_mdh50lm-userdebug \
-    twrp_mdh50lm-eng
+    lineage_mdh50lm-user \
+    lineage_mdh50lm-userdebug \
+    lineage_mdh50lm-eng
