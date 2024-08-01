3b790b1dd34148ee26067ffdef404c7ca7638125
LostOuija <106669288+LostOuija@users.noreply.github.com>
2024-07-31 18:05:23 -0700

    Update and rename lineage_mdh50lm.mk to twrp_mdh50lm.mk

--- twrp_mdh50lm.mk
+++ twrp_mdh50lm.mk
@@ -9,13 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/twrp/config/common_full_phone.mk)
 
 # Inherit from mdh50lm device
 $(call inherit-product, device/lge/mdh50lm/device.mk)
 
 PRODUCT_DEVICE := mdh50lm
-PRODUCT_NAME := lineage_mdh50lm
+PRODUCT_NAME := twrp_mdh50lm
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LM-Q730
 PRODUCT_MANUFACTURER := lge
