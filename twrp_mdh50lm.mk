2db961eafdf08bb5f2ed6715371a7a2eb3025065
LostOuija <106669288+LostOuija@users.noreply.github.com>
2024-08-05 16:21:07 -0700

    Update twrp_mdh50lm.mk

--- twrp_mdh50lm.mk
+++ twrp_mdh50lm.mk
@@ -9,8 +9,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+
 
 # Inherit from mdh50lm device
 $(call inherit-product, device/lge/mdh50lm/device.mk)
