5cc8365537cc50b6f36998e19dc8fdf0da64baee
LostOuija <106669288+LostOuija@users.noreply.github.com>
2024-08-05 15:09:20 -0700

    Update twrp_mdh50lm.mk

--- twrp_mdh50lm.mk
+++ twrp_mdh50lm.mk
@@ -1,5 +1,6 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,13 +9,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
-# Virtual A/B OTA
-# $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
-# Inherit some common Lineage stuff.
-
+# Inherit some common Twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from mdh50lm device
 $(call inherit-product, device/lge/mdh50lm/device.mk)
